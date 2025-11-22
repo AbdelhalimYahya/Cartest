6160b0cae09fea96f830676f8d6ca2b680bd71bb
abdelhalim <abdelhalim@gmail.com>
2025-11-22 20:03:24 +0200

    update the backend part

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -1,6 +1,6 @@
 import Redis from "ioredis";
 import dotenv from "dotenv";
-
 dotenv.config();
 
-export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+export const redis = new Redis("rediss://default:AZxrAAIncDIzYmFhY2IxMDJlZWY0MWI4OWFiZmEzOWRjZTI5NDU1ZHAyNDAwNDM@proven-hookworm-40043.upstash.io:6379");
+// await redis.set('foo', 'bar');
